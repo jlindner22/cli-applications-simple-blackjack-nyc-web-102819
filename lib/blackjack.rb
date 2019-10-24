010a0e8d29f7730d9e54361eb3104ad190bd6f52
Jen Lindner <jenlindner22@gmail.com>
2019-10-24 19:33:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,7 @@ gets.chomp
 end
 
 def end_game
-prints "Sorry, you lose. Card total: #{display_card_total}. Thank you for playing."
+prints "Sorry, you lose. Card total: #{card_total}. Thank you for playing."
 end
 
 def initial_round
